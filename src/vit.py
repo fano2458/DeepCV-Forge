de39e02ecabf67b0c15a560859c9cb8fd5aaa923
fano2458 <fano2458@gmail.com>
2024-07-17 16:10:12 +0500

    vit implementation

--- src/vit.py
+++ src/vit.py
@@ -2,96 +2,182 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 
+import math
+
 from dataclasses import dataclass
 
 
 @dataclass
 class ViTConfig:
     n_layer: int = 12
-    n_embd: int = 192
-    n_head: int = 12
+    d_size: int = 192
+    n_head: int = 3
     n_class: int = 1000
+    patch_size: int = 16
+    image_size: int = 224
+    num_channels: int = 3
 
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-# TODO finish implementation
+class ViTPatchEmbedding(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+        self.projection = nn.Conv2d(self.config.num_channels, self.config.d_size, 
+                                    kernel_size=self.config.patch_size, stride=self.config.patch_size)
+
+    def forward(self, x):
+        x = self.projection(x).flatten(2).transpose(1, 2)
+        return x
+
 
 class ViTEmbeddings(nn.Module):
     def __init__(self, config):
         super().__init__()
 
-        self.config = config
+        num_patches = (config.image_size // config.patch_size) ** 2
 
-    def forward(self):
-        pass
+        self.config = config
+        self.cls_token = nn.Parameter(torch.rand(1, 1, self.config.d_size))
+        self.position_embeddings = nn.Parameter(torch.rand(1, num_patches+1, self.config.d_size))
+        self.patch_embeddings = ViTPatchEmbedding(config)
+
+
+    def forward(self, x):
+        cls_token = self.cls_token.expand(x.shape[0], -1, -1)                  # add cls_token
+        # print(torch.cat((cls_token,self.patch_embeddings(x)), dim=1).shape)
+        embeddings = torch.cat((cls_token, self.patch_embeddings(x)), dim=1) + self.position_embeddings  # make positional embeddings
+
+        return embeddings
+
 
 
 class ViTSelfAttention(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
 
         self.config = config
-        self.query = None
-        self.key = None
-        self.value = None
+        self.query = nn.Linear(self.config.d_size, self.config.d_size, bias=True)
+        self.key = nn.Linear(self.config.d_size, self.config.d_size, bias=True)
+        self.value = nn.Linear(self.config.d_size, self.config.d_size, bias=True)
 
-    def forward(self):
-        pass
+    def transpose_for_scores(self, x):
+        new_x_shape = x.size()[:-1] + (self.config.n_head, self.config.d_size // self.config.n_head)
+        x = x.view(new_x_shape)       # (B, N_embd, N_head, D_head)
+
+        return x.permute(0, 2, 1, 3)  # (B, N_head, N_embd, D_head)
+
+    def forward(self, x):
+        key_layer = self.transpose_for_scores(self.key(x))
+        query_layer = self.transpose_for_scores(self.query(x))
+        value_layer = self.transpose_for_scores(self.value(x))
+
+        attention_scores = torch.matmul(query_layer, key_layer.transpose(-1, -2))
+        attention_scores = attention_scores / math.sqrt(self.config.d_size // self.config.n_head)
+
+        attention_probs = F.softmax(attention_scores, dim=-1)
+
+        context_layer = torch.matmul(attention_probs, value_layer)
+        context_layer = context_layer.permute(0, 2, 1, 3).contiguous()
+        new_context_layer_shape = context_layer.size()[:-2] + (self.config.d_size,)
+        context_layer = context_layer.view(new_context_layer_shape)
+
+        return context_layer
+
+
+class ViTSelfOutput(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+
+        self.config = config
+        self.dense = nn.Linear(self.config.d_size, self.config.d_size)
+
+    def forward(self, x):
+        return self.dense(x)
 
 
 class ViTLayer(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
 
         self.config = config
         self.attention = nn.ModuleDict(dict(
             attention = ViTSelfAttention(self.config),
-            output = None,                                    # TODO
+            output = ViTSelfOutput(self.config),
         ))
         self.intermediate = nn.ModuleDict(dict(
-            dense = nn.Linear(self.config.n_embd, self.config.n_embd * 4),
-            act_fn = nn.GELU()                                # TODO check
+            dense = nn.Linear(self.config.d_size, self.config.d_size * 4),
+            act_fn = nn.GELU()
         ))
-        self.output = nn.Linear(self.config.n_embd * 4, self.config.n_embd)
-        self.layernorm_before = nn.LayerNorm(self.config.n_embd)
-        self.layernorm_after = nn.LayerNorm(self.config.n_embd)
+        self.output = nn.Linear(self.config.d_size * 4, self.config.d_size)
+        self.layernorm_before = nn.LayerNorm(self.config.d_size)
+        self.layernorm_after = nn.LayerNorm(self.config.d_size)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        x = self.attention.output(self.attention.attention(self.layernorm_before(x))) + x                  # layer norm before self attention + skip connection
+        x = self.output(self.intermediate.act_fn(self.intermediate.dense((self.layernorm_after(x))))) + x   # layer norm after self attention + skip connection
+                 
+        return x
 
 
 class ViTEncoder(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
 
         self.config = config
         self.layer = nn.ModuleList([ViTLayer(self.config) for _ in range(self.config.n_layer)])
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        for layer in self.layer:
+            x = layer(x)
+        
+        return x
 
 
 class ViT(nn.Module):
     def __init__(self, config):
-        super.__init__()
+        super().__init__()
 
         self.config = config
 
         self.vit = nn.ModuleDict(dict(
             embeddings = ViTEmbeddings(self.config),
             encoder = ViTEncoder(self.config),
-            layernorm = nn.LayerNorm(self.config.n_embd),
+            layernorm = nn.LayerNorm(self.config.d_size),
         ))
 
-        self.classifier = nn.Linear(self.config.n_embd, self.config.n_class, bias=True)
+        self.classifier = nn.Linear(self.config.d_size, self.config.n_class, bias=True)
 
-    def forward(self):
-        pass
+    def forward(self, x):
+        # x = self.vit(x)
+        x = self.vit.embeddings(x)
+        # print("after embd", x.shape)
+        x = self.vit.encoder(x)
+        # print("x after encoder is ", x.shape)
+        x = x[:, 0, :]
+        # print("cls token extracted ", x.shape)
+        x = self.vit.layernorm(x)
+        x = self.classifier(x)
+
+        return x
+
 
     @staticmethod
     def from_pretrained(self):
         pass
 
     
+# attn = ViTSelfAttention(ViTConfig())
+
+# print(attn.transpose_for_scores(torch.zeros(1, 197, 192)).shape)
+
+model = ViT(ViTConfig())
+
+test_tensor = torch.rand(1, 3, 224, 224)
+
+out = model(test_tensor)
+
+print(out.shape)
